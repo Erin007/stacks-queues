0ef306f9427739258f9f45c20d6f2c3dc9ad812f
Erin Bond <erin@erinebond.com>
2016-08-25 10:36:25 -0700

    completed implementation

--- Stack.rb
+++ Stack.rb
@@ -1,20 +1,26 @@
 class Stack
   def initialize
-    @store = Array.new
+    @store = Array.new #<--- underlying structure for the stack
   end
-  
+
   def push(element)
+    @store << element #@store.push(element)
   end
- 
+
   def pop
+      return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 #calls the method size from before 
+    #@store.empty? #@store.length == 0
   end
 end
